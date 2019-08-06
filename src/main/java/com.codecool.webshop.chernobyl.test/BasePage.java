f5ad062fb0842c09344546a4408cd8228db916dd
gaborBKI <gabor.bki@gmail.com>
2019-08-06 13:07:33 +0200

    Created driver factory.

--- src/main/java/com.codecool.webshop.chernobyl.test/BasePage.java
+++ src/main/java/com.codecool.webshop.chernobyl.test/BasePage.java
@@ -4,10 +4,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public abstract class BasePage {
-    protected WebDriver driver = new FirefoxDriver();
+    /*
+
+    Accepts a System.getenv("BROWSER") value. The environment variable can be:
+        FIREFOX or
+        CHROME
+
+     */
+
+    private WebDriver driver = DriverFactory.getWebDriver(System.getenv("BROWSER"));
 
     public void open(String url){
         driver.get(url);
+    }
+
+    public static void main(String[] args) {
 
     }
 }
---
+++ src/main/java/com.codecool.webshop.chernobyl.test/DriverFactory.java
@@ -0,0 +1,23 @@
+package com.codecool.webshop.chernobyl.test;
+
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver getWebDriver(String browser) {
+
+        switch (browser) {
+            case "CHROME":
+                return new ChromeDriver();
+            case "FIREFOX":
+                return new FirefoxDriver();
+            case "IE":
+                throw new IllegalArgumentException("C'mon nobody uses IE");
+            default:
+                throw new InvalidArgumentException("Sorry " + browser + " is not an option.");
+        }
+    }
+}
